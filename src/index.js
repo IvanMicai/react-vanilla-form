0ebd6a34e6ec24095ba60aff2d4110d874fc5bbd
Ivan Micai da Silva <ivanmicai@gmail.com>
2018-07-12 11:45:57 -0300

    Artis diff

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react'
-import {
-  node,
-  object,
-  oneOf,
-  func,
-  string,
-} from 'prop-types'
+import PropTypes from 'prop-types'
 
 import {
   anyPass,
+  assocPath,
   ap,
   complement,
   contains,
   defaultTo,
-  dissocPath,
-  dropLast,
   equals,
   filter,
   is,
@@ -24,20 +17,15 @@ import {
   partial,
   partialRight,
   pathEq,
-  pathSatisfies,
   reduce,
   reject,
   set,
-  merge,
   view,
 } from 'ramda'
 
 const isErrorEmpty = anyPass([isNil, isEmpty, complement(Boolean)])
 
-const capitalize = str =>
-  str && `${str[0].toUpperCase()}${str.slice(1)}`
-
-const getValue = event => {
+const getValue = (event) => {
   if (event.target) {
     return contains(event.target.value, ['on', 'off'])
       ? event.target.checked
@@ -47,12 +35,6 @@ const getValue = event => {
   return event
 }
 
-const isCheckable = element =>
-  element.props.type === 'radio' ||
-  element.props.type === 'checkbox' ||
-  typeof element.props.checked !== 'undefined' ||
-  typeof (element.type.defaultProps || {}).checked !== 'undefined'
-
 const defaultToEmptyString = defaultTo('')
 
 export default class Form extends Component {
@@ -60,37 +42,26 @@ export default class Form extends Component {
     super(props)
 
     this.state = {
-      data: props.data || {},
       errors: props.errors || {},
+      data: props.data || {},
     }
 
     this.cloneTree = this.cloneTree.bind(this)
     this.validateTree = this.validateTree.bind(this)
-    this.validateElement = this.validateElement.bind(this)
     this.notifyChangeEvent = this.notifyChangeEvent.bind(this)
-    this.handleEvent = this.handleEvent.bind(this)
+    this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
-
-    if (props.validateDataProp) {
-      this.state.errors = this.validateTree(this.state.errors, this)
-    }
   }
 
   componentWillReceiveProps (nextProps) {
     const { data, errors } = nextProps
 
     if (data && !equals(data, this.props.data)) {
-      this.setState({ data }, () => {
-        if (this.props.validateDataProp) {
-          const errors = this.validateTree(this.state.errors, this)
-          this.setState({ errors })
-        }
-      })
-      return
+      this.setState({ data })
     }
 
     if (errors && !equals(errors, this.props.errors)) {
-      this.setState({ errors: merge(this.state.errors, errors) })
+      this.setState({ errors })
     }
   }
 
@@ -103,153 +74,140 @@ export default class Form extends Component {
     }
   }
 
-  validateElement (path, data, errors) {
+  handleChange (path, originalOnChange, event) {
     const lens = lensPath(path)
-    const validation = view(lens, this.props.validation)
+    const value = getValue(event)
 
-    if (!validation) {
-      return errors
+    const data = set(lens, value, this.state.data)
+    const validate = view(lens, this.props.validation)
+
+    if (typeof originalOnChange === 'function') {
+      originalOnChange(event)
     }
 
-    const value = view(lens, data)
+    if (typeof validate === 'function') {
+      const error = validate(defaultToEmptyString(view(lens, data)))
+      const errors = set(lens, error, this.state.errors)
 
-    if (is(Array, validation)) {
-      for (let validate of validation) {
-        const err = validate(value)
+      this.setState({ data, errors }, this.notifyChangeEvent)
+    }
 
-        if (!isErrorEmpty(err)) {
-          return set(lens, err, errors)
-        }
+    if (validate && validate.constructor === Array) {
+      const validationErrors = reject(
+        complement(Boolean),
+        ap(validate, [value])
+      )
+
+      if (validationErrors.length > 0) {
+        const error = validationErrors[0]
+        const errors = set(lens, error, this.state.errors)
+
+        this.setState({ data, errors }, this.notifyChangeEvent)
+        return
       }
-    }
 
-    else if (typeof validation === 'function') {
-      const err = validation(value)
+      const errors = set(lens, null, this.state.errors)
 
-      if (!isErrorEmpty(err)) {
-        return set(lens, err, errors)
-      }
-    }
-
-    const parentPath = dropLast(1, path)
-
-    if (parentPath.length > 0) {
-      const parentErrors = view(lensPath(parentPath), errors)
-
-      if (pathSatisfies(isErrorEmpty, parentPath, errors)) {
-        return dissocPath(parentPath, errors)
-      }
-    }
-
-    return dissocPath(path, errors)
-  }
-
-  handleEvent (eventName, path, originalHandler, event) {
-    if (typeof originalHandler === 'function') {
-      originalHandler(event)
-    }
-
-    if (event && event.defaultPrevented) {
+      this.setState({ data, errors }, this.notifyChangeEvent)
       return
     }
 
-    const { validateOn } = this.props
-
-    let data = this.state.data
-    let errors = this.state.errors
-
-    if (eventName === 'change') {
-      const lens = lensPath(path)
-      const value = getValue(event)
-
-      data = set(lens, value, this.state.data)
-    }
-
-    if (eventName === validateOn) {
-      errors = this.validateElement(path, data, errors)
-    }
-
-    this.setState({ data, errors }, this.notifyChangeEvent)
+    this.setState({ data }, this.notifyChangeEvent)
   }
 
-  cloneTree (element, index, parentPath = []) {
+  cloneTree (element, index, parentElement, parentPath = []) {
     if (!element || typeof element === 'string') {
       return element
     }
 
-    const path = element.props.name
-      ? [...parentPath, element.props.name]
-      : parentPath
-
-    if (element.type === 'fieldset') {
-      return React.cloneElement(element, {}, React.Children.map(
-        element.props.children,
-        partialRight(this.cloneTree, [path])
-      ))
-    }
-
     if (element.props.role === 'alert' && element.props.htmlFor) {
-      const lens = lensPath([...path, element.props.htmlFor])
+      const path = [...parentPath, element.props.htmlFor]
+      const lens = lensPath(path)
+
       const errors = view(lens, this.state.errors)
 
       if (errors) {
-        const children = is(Array, errors) ? errors[0] : errors
-        return React.cloneElement(element, { children })
+        const error = is(Array, errors) ? errors[0] : errors
+
+        return React.cloneElement(
+          element,
+          { children: error }
+        )
       }
 
       return element
     }
 
+    const name = isNil(element.props.name) ? [] : [element.props.name]
+    const path = [...parentPath, ...name]
+
     if (element.props.name) {
+      const originalOnChange = element.props.onChange
+      const { customErrorProp } = this.props
       const lens = lensPath(path)
-
-      const {
-        validateOn,
-        customErrorProp: errorProp = 'error',
-      } = this.props
-
-      let props = {}
-
-      props.onChange = partial(
-        this.handleEvent,
-        ['change', path, element.props.onChange]
-      )
-
-      if (['blur', 'focus'].includes(validateOn)) {
-        const eventProp = `on${capitalize(validateOn)}`
-
-        props[eventProp] = partial(
-          this.handleEvent,
-          [validateOn, path, element.props[eventProp]]
-        )
-      }
-
       const value = view(lens, this.state.data)
+      const error = customErrorProp
+        ? { [customErrorProp]: view(lens, this.state.errors) }
+        : {}
+      const onChange = element.type !== 'fieldset'
+        ? { onChange: partial(this.handleChange, [path, originalOnChange]) }
+        : {}
 
-      if (isCheckable(element)) {
-        if (typeof value === 'boolean') {
-          props.checked = value
-        } else {
-          props.checked = value === element.props.value
+      if (parentElement) {
+        const siblings = filter(
+          pathEq(['props', 'name'], element.props.name),
+          parentElement.props.children
+        )
+
+        if (siblings.length > 1) {
+          return React.cloneElement(element, {
+            ...error,
+            checked: value === element.props.value,
+            ...onChange,
+          })
         }
-      } else {
-        props.value = value
       }
 
-      const error = view(lens, this.state.errors)
+      if (element.props.children) {
+        const children = React.Children.map(
+          element.props.children,
+          partialRight(this.cloneTree, [element, path])
+        )
 
-      if (error) {
-        props[errorProp] = error
+        if (typeof value === 'object') {
+          return React.cloneElement(element, { children })
+        }
+
+        return React.cloneElement(element, {
+          ...error,
+          ...onChange,
+          value,
+          children,
+        })
       }
 
-      return React.cloneElement(element, props)
+      if (is(Boolean, value)) {
+        return React.cloneElement(element, {
+          ...error,
+          checked: value,
+          ...onChange,
+        })
+      }
+
+      return React.cloneElement(element, {
+        ...error,
+        value,
+        ...onChange,
+      })
     }
 
     if (element.props.children) {
-      return React.cloneElement(element, {}, React.Children.map(
-        element.props.children,
-        partialRight(this.cloneTree, [path])
-      ))
+      return React.cloneElement(element, {
+        children: React.Children.map(
+          element.props.children,
+          partialRight(this.cloneTree, [element, path])
+        ),
+      })
     }
 
     return element
@@ -260,33 +218,63 @@ export default class Form extends Component {
       return errors
     }
 
-    const children = React.Children.toArray(element.props.children)
-    const path = element.props.name
-      ? [...parentPath, element.props.name]
-      : parentPath
+    if (!element.props) {
+      return errors
+    }
 
-    if (element.type !== 'select' && children.length > 0) {
-      const childErrors = reduce(
+    const children = React.Children.toArray(element.props.children)
+
+    if (children.length) {
+      const path = element.props.name
+        ? [...parentPath, element.props.name]
+        : parentPath
+
+      return reduce(
         partialRight(this.validateTree, [path]),
         errors,
         children
       )
-
-      if (isErrorEmpty(childErrors)) {
-        const parentPath = dropLast(1, path)
-
-        if (parentPath.length === 0) {
-          return {}
-        }
-
-        return dissocPath(parentPath, errors)
-      }
-
-      return childErrors
     }
 
     if (element.props.name) {
-      return this.validateElement(path, this.state.data, errors)
+      const path = [...parentPath, element.props.name]
+      const lens = lensPath(path)
+      const validation = view(lens, this.props.validation)
+
+      if (!validation) {
+        return errors
+      }
+
+      const value = defaultTo('', view(lens, this.state.data))
+
+      if (is(Array, validation)) {
+        const validationErrors = reject(
+          complement(Boolean),
+          ap(validation, [value])
+        )
+
+        if (validationErrors.length > 0) {
+          const error = validationErrors[0]
+
+          if (!isErrorEmpty(error)) {
+            return assocPath(path, error, errors)
+          }
+        }
+
+        return errors
+      }
+
+      const validationError = validation(value)
+
+      if (isErrorEmpty(validationError)) {
+        return errors
+      }
+
+      return assocPath(
+        path,
+        validationError,
+        errors
+      )
     }
 
     return errors
@@ -294,12 +282,13 @@ export default class Form extends Component {
 
   handleSubmit (event) {
     event.preventDefault()
+    event.stopPropagation()
 
     const errors = this.validateTree({}, this)
 
     this.setState(
       { errors },
-      () => isErrorEmpty(this.state.errors)
+      () => isErrorEmpty(this.state.errors) // eslint-disable-line
         ? this.props.onSubmit(this.state.data)
         : this.props.onSubmit(this.state.data, this.state.errors)
     )
@@ -320,65 +309,23 @@ export default class Form extends Component {
 }
 
 Form.propTypes = {
-  /**
-   * The children can contain any kind of component. Inputs with name
-   * property will be tracked for changes using `onChange` callback.
-   * Sibling labels with `role=alert` and `htmlFor` pointing to a validated
-   * component will be used to present the error message.
-  **/
-  children: node,
-  /**
-   * The validation object whose keys mirror form field structure.
-   * Values of this object can be either functions or a function array.
-   * Validation functions receives the input string and should return
-   * a string message on error and a falsy value otherwise.
-  **/
-  validation: object, // eslint-disable-line
-  /**
-  * The event where validation will be triggered. By default, field
-  * validations runs on `change` event.
-  **/
-  validateOn: oneOf(['change', 'blur', 'focus']),
-  /**
-   * The form submit callback. Receives the serialized form as an object.
-  **/
-  onSubmit: func,
-  /**
-   * The form change callback. This callback runs on every form control's
-   * `onChange`, right after validations. When this is defined, the form
-   * behaves as a controlled component, and the user is responsible for
-   * updating the form state via `data` prop.
-  **/
-  /**
-   * @callback onChange
-   * @param {object} data
-   * @param {object} errors
-  **/
-  onChange: func,
-  /**
-   * The form data object whose keys mirror form field structure.
-   * Setting this prop will set the form controls' values accordingly.
-   * This can be used for rendering an initial state or to use the form
-   * as a controlled component.
-  **/
-  data: object, // eslint-disable-line
-  /**
-   * A property name to inject the error message in the named field.
-   * This is useful for input wrappers with builtin error label, commonly
-   * found in UI libraries.
-  **/
-  customErrorProp: string,
-  className: string,
+  children: PropTypes.node,
+  className: PropTypes.string,
+  customErrorProp: PropTypes.string,
+  data: PropTypes.object, // eslint-disable-line
+  errors: PropTypes.object, // eslint-disable-line
+  onChange: PropTypes.func,
+  onSubmit: PropTypes.func,
+  validation: PropTypes.object, // eslint-disable-line
 }
 
 Form.defaultProps = {
-  children: undefined,
+  children: null,
   className: '',
   customErrorProp: undefined,
-  data: undefined,
-  onChange: undefined,
-  onSubmit: undefined,
-  validateDataProp: false,
-  validateOn: 'change',
+  data: null,
+  errors: null,
+  onChange: null,
+  onSubmit: () => undefined,
   validation: {},
 }
